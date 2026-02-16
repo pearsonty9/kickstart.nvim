ff2b854045978d400c5728ad95333dbdcb3e3a18
Tyler Pearson <pearsonty9@gmail.com>
2026-02-16 12:54:24 -0500

    use prettier as formatter for typescript

--- init.lua
+++ init.lua
@@ -773,6 +773,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
